59d39f816f322e5de1cc21645f3e94ec522a2e59
Ian Lohan <ianlohan.acc@gmail.com>
2024-11-04 20:17:28 -0300

    CRUD Usuarios

--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff33c2a8d82ffa6493ae49e95874fa03a768c5d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1dda8e2068987c89e1b50f5698cb2a64b00ea0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
